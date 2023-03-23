d9132978a2e3b08c35b653677132e17b073c7a67
Raj Ratan <raj808341@gmail.com>
2023-03-23 09:08:48 +0530

    Problem that Context API solves

---
+++ five- contextAPI/side-one-context/src/Problem.js
@@ -0,0 +1,31 @@
+// import React, { useState } from "react";
+
+// const GrandChild = (props) => {
+//   return (
+//     <div>
+//       <h1>Grand Child : </h1>
+//       <Child brand={props.brand} />
+//     </div>
+//   );
+// };
+
+// const Child = (props) => {
+//   return (
+//     <div>
+//       <h1>Child : {props.brand}</h1>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const [brandName] = useState("Amazon");
+
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <GrandChild brand={brandName} />
+//     </div>
+//   );
+// };
+
+// export default App;
