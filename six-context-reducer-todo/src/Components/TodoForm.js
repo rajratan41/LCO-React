55221a609c04b6ecdcf38881d4648bcfba6821e8
Raj Ratan <raj808341@gmail.com>
2023-03-23 20:11:03 +0530

    Sending a Dispatch

--- six-context-reducer-todo/src/Components/TodoForm.js
+++ six-context-reducer-todo/src/Components/TodoForm.js
@@ -7,15 +7,40 @@ import { TodoContext } from "../context/TodoContext";
 import { ADD_TODO } from "../context/action.types";
 
 const TodoForm = () => {
+  const [todoString, setTodoString] = useState("");
+  const { dispatch } = useContext(TodoContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todoString === "") {
+      return alert("Please Enter a Todo");
+    }
+
+    const todo = {
+      todoString,
+      id: v4(),
+    };
+
+    dispatch({
+      type: ADD_TODO,
+      payload: todo,
+    });
+
+    setTodoString("");
+  };
+
   return (
-    <Form className="form">
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <InputGroup>
           <Input
             type="text"
             name="todo"
             id="todo"
-            placeholder="Your Next Todo" /*Todo: value, onClick*/
+            placeholder="Your Next Todo"
+            value={todoString}
+            onChange={(e) => setTodoString(e.target.value)}
           />
           <Button color="warning" /**Todo: Onclick */>Add</Button>
         </InputGroup>
