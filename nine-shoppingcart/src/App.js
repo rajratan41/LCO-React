d4cb5eebb6774fd9d3b91cfd484a51810d7f649d
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-04-17 11:59:18 +0000

    bring the shop app together

--- nine-shoppingcart/src/App.js
+++ nine-shoppingcart/src/App.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+
+import { Container, Row, Col } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+
 import BuyPage from "./Components/BuyPage";
+import Cart from "./Components/Cart";
 
 function App() {
   const [cartItem, setCartItem] = useState([]);
@@ -35,9 +39,17 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <BuyPage addInCart={addInCart} />
-    </div>
+    <Container fluid>
+      <ToastContainer />
+      <Row>
+        <Col md="8">
+          <BuyPage addInCart={addInCart} />
+        </Col>
+        <Col md="4">
+          <Cart cartItem={cartItem} removeItem={removeItem} buyNow={buyNow} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
