c6aba88d2fe6fb035f57f37e33be1df50c01f3c9
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-04-17 09:31:34 +0000

    add item to the cart

--- nine-shoppingcart/src/App.js
+++ nine-shoppingcart/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [cartItem, setCartItem] = useState([]);
+
+  const addInCart = (item) => {
+    const isAlreadyAdded = cartItem.findIndex(function (array) {
+      return array.id === item.id;
+    });
+
+    if (isAlreadyAdded !== -1) {
+      toast("already added in Cart", {
+        type: "error",
+      });
+    }
+
+    setCartItem([...cartItem, item]);
+  };
+
+  return <div className="App"></div>;
 }
 
 export default App;
