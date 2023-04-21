bfaa5b64585cd099b729fd61851766210ef01b7f
Raj Ratan <raj808341@gmail.com>
2023-04-21 20:50:25 +0530

    updated header

--- ten-firebase-github-app/src/components/Header.js
+++ ten-firebase-github-app/src/components/Header.js
@@ -17,7 +17,25 @@ import { UserContext } from "../context/UserContext";
 const Header = () => {
   return (
     <Navbar color="info" light expand="md">
-      <NavbarBrand>Gitfire App</NavbarBrand>
+      <NavbarBrand>
+        <Link to="/" className="text-white">
+          Gitfire App
+        </Link>
+      </NavbarBrand>
+      <NavbarToggler />
+      <Collapse navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink className="text-white">Sign Up</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="text-white">Sign In</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="text-white">Log Out</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
     </Navbar>
   );
 };
