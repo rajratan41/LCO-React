970a7cd6f1f6706871e434da4c35f0a0bf8161d1
Raj Ratan <raj808341@gmail.com>
2023-04-29 11:13:28 +0530

    change app name

--- ten-firebase-github-app/src/components/Header.js
+++ ten-firebase-github-app/src/components/Header.js
@@ -25,7 +25,7 @@ const Header = () => {
     <Navbar color="info" light expand="md">
       <NavbarBrand>
         <Link to="/" className="text-white">
-          LCO gitfire app
+           Gitfire App
         </Link>
       </NavbarBrand>
       <NavbarText className="text-white">
