44b20dd11e534ee31f34fdda40f7238dfe0e527a
Raj Ratan <raj808341@gmail.com>
2023-04-29 10:16:41 +0530

    added firebase config

---
+++ ten-firebase-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyATuZonfXpdwtFGvHIw8NQG3eaNHYyb8_I",
+    authDomain: "mygitapp-c8838.firebaseapp.com",
+    projectId: "mygitapp-c8838",
+    storageBucket: "mygitapp-c8838.appspot.com",
+    messagingSenderId: "767586570669",
+    appId: "1:767586570669:web:45c537ae5c5d78fae99124",
+    measurementId: "G-646T18H5PQ"
+  };
